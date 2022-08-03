64eb3c6305d3fb988a7ccca12fbaed25a7b2daab
JakJar <kuba.jaroszewicz@gmail.com>
2022-08-03 15:16:08 +0200

    feat: added methods to check winning conditions - HorizontalWin, VerticalWin, DiagonalWin

--- TicTacToeGame/TicTacToeGame/Game.cs
+++ TicTacToeGame/TicTacToeGame/Game.cs
@@ -50,6 +50,53 @@
             return 'O';
         }
 
+        public void HorizontalWin()
+        {
+            char[] players = { 'X', 'O' };
+            char[] gameBoard = board.GetGameBoard();
+
+            foreach (var player in players)
+            {
+                if ((gameBoard[0] == player && gameBoard[1] == player && gameBoard[2] == player) ||
+                    (gameBoard[3] == player && gameBoard[4] == player && gameBoard[5] == player) ||
+                    (gameBoard[6] == player && gameBoard[7] == player && gameBoard[8] == player))
+                {
+                    Console.WriteLine($"Player {player} win horizontally!");
+                }
+            }
+        }
+
+        public void VerticalWin()
+        {
+            char[] players = { 'X', 'O' };
+            char[] gameBoard = board.GetGameBoard();
+
+            foreach (char player in players)
+            {
+                if ((gameBoard[0] == player && gameBoard[3] == player && gameBoard[6] == player) ||
+                    (gameBoard[1] == player && gameBoard[4] == player && gameBoard[7] == player) ||
+                    (gameBoard[2] == player && gameBoard[5] == player && gameBoard[8] == player))
+                {
+                    Console.WriteLine($"Player {player} win vertically!");
+                }
+            }
+        }
+
+        public void DiagonalWin()
+        {
+            char[] players = { 'X', 'O' };
+            char[] gameBoard = board.GetGameBoard();
+
+            foreach (char player in players)
+            {
+                if ((gameBoard[0] == player && gameBoard[4] == player && gameBoard[8] == player) ||
+                    (gameBoard[6] == player && gameBoard[4] == player && gameBoard[2] == player))
+                {
+                    Console.WriteLine($"Player {player} win diagonally!");
+                }
+            }
+        }
+
         public void Play()
         {
             Console.Clear();
