a25b0036d4d6e6c0f18888c55d5e524a8e3534a5
JakJar <kuba.jaroszewicz@gmail.com>
2022-08-03 11:26:04 +0200

    feat: added MarkField method

--- TicTacToeGame/TicTacToeGame/Board.cs
+++ TicTacToeGame/TicTacToeGame/Board.cs
@@ -8,5 +8,16 @@ namespace TicTacToeGame
 {
     internal class Board
     {
+        private char[] _gameBoard;
+
+        public Board()
+        {
+            _gameBoard = new char[9];
+        }
+
+        public void MarkField(int position, char mark)
+        {
+            _gameBoard[position] = mark;
+        }
     }
 }
