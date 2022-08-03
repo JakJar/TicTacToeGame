a21515b8cf4b650584a7f7d37231daa67731844b
JakJar <kuba.jaroszewicz@gmail.com>
2022-08-03 14:29:53 +0200

    feat: added Game class

---
+++ TicTacToeGame/TicTacToeGame/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace TicTacToeGame
+{
+    internal class Game
+    {
+        private Board board;
+        private int _moves;
+
+        public Game()
+        {
+            board= new Board();
+            _moves = 0;
+        }
+    }
+}
